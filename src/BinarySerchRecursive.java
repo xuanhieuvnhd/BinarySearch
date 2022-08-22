0aef7689c8b6e47e03525b3ea50b2ad9c719fdef
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-08-22 15:47:07 +0700

    22/08

---
+++ src/BinarySerchRecursive.java
@@ -0,0 +1,28 @@
+public class BinarySerchRecursive {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+        int mid = (low + high) / 2;
+        if (high >= low) {
+            if (list[mid] == key) {
+                return mid;
+            } else if (list[mid] > key) {
+                return binarySearch(list, low);
+            } else if (list[mid] < key) {
+                return binarySearch(list, high);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));  /* 0 */
+        System.out.println(binarySearch(list, 11)); /* 4 */
+        System.out.println(binarySearch(list, 79)); /*12 */
+        System.out.println(binarySearch(list, 1));  /*-1 */
+        System.out.println(binarySearch(list, 5));  /*-1 */
+        System.out.println(binarySearch(list, 80)); /*-1 */
+    }
+}
